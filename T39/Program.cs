a8cfec81b6627cd074ccc499da2edf48da0c81a5
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-11 19:37:35 +0300

    Add T39

---
+++ T39/Program.cs
@@ -0,0 +1,59 @@
+﻿﻿// Задача 39: Напишите программу, которая перевернет 
+// одномерный массив )последний элемент будет на первом
+// месте, а первый - на последнем и т.д.
+// [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+// [6, 7, 3, 6] -> [6, 3, 7, 6]
+
+// =======================
+int[] CreateRndArray(int size, int min, int max)
+{
+    int[] rndArray = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rndArray.Length; i ++)
+    {
+        rndArray[i] = rnd.Next(min, max + 1); 
+    }
+    return rndArray;
+}
+// =======================
+void PrintArray(int[] array, string sep = " ")
+{
+    for (int i = 0; i < array.Length; i ++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}{sep} ");
+        else if (i < array.Length - 1) Console.Write($"{array[i]}{sep} ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+// =======================
+
+void ReverseArray(int[] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length / 2; i ++)
+    {
+        temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    
+}
+// =======================
+
+Console.Write("Введите размерность массива :");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите нижнюю границу чисел массива :");
+int arrMinNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите верхнюю границу чисел массива :");
+int arrMaxNum = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateRndArray(arrSize, arrMinNum, arrMaxNum);
+PrintArray(arr, ";");
+
+ReverseArray(arr);
+
+Console.WriteLine();
+PrintArray(arr, ";");
