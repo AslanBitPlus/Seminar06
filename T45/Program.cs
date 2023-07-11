9cdfea3c42359b5930ca4a21a8557c58e5baa992
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-11 21:55:34 +0300

    Add T45

---
+++ T45/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива м помощью поэлементного копирования.
+
+// =======================
+int[] CreateRndArray(int size, int min, int max)
+{
+    int[] rndArray = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rndArray.Length; i ++)
+    {
+        rndArray[i] = rnd.Next(min, max + 1); 
+    }
+    return rndArray;
+}
+// =======================
+void PrintArray(int[] array, string sep = " ")
+{
+    for (int i = 0; i < array.Length; i ++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}{sep} ");
+        else if (i < array.Length - 1) Console.Write($"{array[i]}{sep} ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+// =======================
+
+int[] NewArray(int[] array)
+{
+    int[] array1 = new int[array.Length];
+    for (int i = 0; i < array.Length; i ++)
+    {
+        array1[i] = array[i];
+    }
+    return array1;
+}
+// =======================
+
+Console.Write("Введите размерность массива :");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите нижнюю границу чисел массива :");
+int arrMinNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите верхнюю границу чисел массива :");
+int arrMaxNum = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateRndArray(arrSize, arrMinNum, arrMaxNum);
+PrintArray(arr, ";");
+
+NewArray(arr);
+
+Console.WriteLine();
+PrintArray(NewArray(arr), ";");
