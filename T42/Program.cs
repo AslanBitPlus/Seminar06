7acd35d1d5b41e542fdf4515a756121ba749c0ae
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-11 21:14:22 +0300

    Add T42

---
+++ T42/Program.cs
@@ -0,0 +1,58 @@
+﻿﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+// =======================
+int[] DecToBinArr(int num)
+{
+    int digCount = 0;
+    int num1 = num;
+    while (num1 != 0)
+    {
+        num1 = num1 / 2;
+        digCount += 1;
+    }
+
+    int[] digitArray = new int[digCount];
+ while (num != 0)
+    {
+        digitArray[digCount - 1] = num % 2;
+        num = num / 2;
+        digCount -= 1;
+    }    
+
+
+    return digitArray;
+}
+
+// =======================
+string DecToBinStr(int num)
+{
+    string result = string.Empty;
+    while (num != 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result; 
+}
+
+// =======================
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i ++)
+        Console.Write($"{array[i]}");
+}
+// =======================
+
+Console.Write("Введите десятичное число: ");
+int decNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = DecToBinArr(decNumber);
+
+PrintArray(arr);
+
+Console.WriteLine();
+Console.Write(DecToBinStr(decNumber));
